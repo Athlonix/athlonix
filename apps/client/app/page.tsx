6177c009f33b5b3fa7c037df52c5a42874712fff
Metololo <93775692+Metololo@users.noreply.github.com>
2024-04-07 21:09:47 +0200

    feat: set color pallet for shadcn

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,9 +1,11 @@
-import { Button } from '@repo/ui/components/button';
+import { Button } from "@repo/ui/components/button";
 
 export default function Page(): JSX.Element {
-  return (
-    <main>
-      <Button>Click me</Button>
-    </main>
-  );
+    return (
+        <main>
+            <Button variant={"default"}>Click me</Button>
+            hello this is tesxt
+            <p>hello this is other text</p>
+        </main>
+    );
 }

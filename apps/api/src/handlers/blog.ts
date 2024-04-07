afa955e7ab6bc69b73834faf910d9e5137536f3b
Antony David <antonydavid945@gmail.com>
2024-04-07 19:55:08 +0200

    feat(api): setup basic blog routes (#38)

---
+++ apps/api/src/handlers/blog.ts
@@ -0,0 +1,121 @@
+import { zValidator } from '@hono/zod-validator';
+import { Hono } from 'hono';
+import { HTTPException } from 'hono/http-exception';
+import { z } from 'zod';
+import { supabase } from '../libs/supabase.js';
+import authMiddleware from '../middlewares/auth.js';
+
+export const blog = new Hono();
+
+blog.get('/posts', authMiddleware, async (c) => {
+  const { data, error } = await supabase.from('POSTS').select('*');
+
+  if (error) {
+    throw new HTTPException(500, { message: error.message });
+  }
+
+  return c.json(data, 200);
+});
+
+blog.get(
+  '/posts/:postId',
+  authMiddleware,
+  zValidator(
+    'param',
+    z.object({
+      postId: z.coerce.number().min(1),
+    }),
+  ),
+  async (c) => {
+    const { postId } = c.req.valid('param');
+    const { data, error } = await supabase.from('POSTS').select('*').eq('id', postId).single();
+
+    if (error) {
+      throw new HTTPException(500, { message: error.message });
+    }
+    if (!data) {
+      return c.json({ message: 'Post not found' }, 404);
+    }
+
+    return c.json(data, 200);
+  },
+);
+
+blog.post(
+  '/posts',
+  authMiddleware,
+  zValidator(
+    'json',
+    z.object({
+      title: z.string(),
+      content: z.string(),
+    }),
+  ),
+  async (c) => {
+    const { title, content } = c.req.valid('json');
+    const { data, error } = await supabase.from('POSTS').insert({ title, content }).select().single();
+
+    if (error) {
+      throw new HTTPException(500, { message: error.message });
+    }
+
+    return c.json(data, 201);
+  },
+);
+
+blog.delete(
+  '/posts/:postId',
+  authMiddleware,
+  zValidator(
+    'param',
+    z.object({
+      postId: z.coerce.number().min(1),
+    }),
+  ),
+
+  async (c) => {
+    const { postId } = c.req.valid('param');
+    const { error, count } = await supabase.from('POSTS').delete({ count: 'exact' }).eq('id', postId);
+
+    if (error) {
+      throw new HTTPException(500, { message: error.message });
+    }
+    if (count === 0) {
+      return c.json({ message: 'Post not found' }, 404);
+    }
+
+    return c.json({ message: 'Post deleted successfully' }, 200);
+  },
+);
+
+blog.patch(
+  '/posts/:postId',
+  authMiddleware,
+  zValidator(
+    'param',
+    z.object({
+      postId: z.coerce.number().min(1),
+    }),
+  ),
+  zValidator(
+    'json',
+    z.object({
+      title: z.string(),
+      content: z.string(),
+    }),
+  ),
+  async (c) => {
+    const { postId } = c.req.valid('param');
+    const { title, content } = c.req.valid('json');
+    const { data, error } = await supabase.from('POSTS').update({ title, content }).eq('id', postId).select().single();
+
+    if (error) {
+      throw new HTTPException(500, { message: error.message });
+    }
+    if (!data) {
+      return c.json({ message: 'Post not found' }, 404);
+    }
+
+    return c.json(data, 200);
+  },
+);
--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,21 +1,33 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 import { compress } from 'hono/compress';
+import { HTTPException } from 'hono/http-exception';
+import { logger } from 'hono/logger';
 import { prettyJSON } from 'hono/pretty-json';
 import { secureHeaders } from 'hono/secure-headers';
 import authRoutes from './handlers/auth.js';
-import authMiddleware from './middlewares/auth.js';
+import { blog } from './handlers/blog.js';
 
 const app = new Hono();
 
+if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+  app.use(logger());
+}
 app.use(prettyJSON());
 app.use(secureHeaders());
 app.use(compress());
 
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    return err.getResponse();
+  }
+  return c.json({ message: 'Internal server error' }, 500);
+});
+
 app.get('/', (c) => c.text('Hello, Hono!', 200));
 app.route('/auth/', authRoutes);
 app.get('/health', (c) => c.json('OK', 200));
-app.get('/protected', authMiddleware, (c) => c.json({ message: 'Protected route' }, 200));
+app.route('/blog', blog);
 
 const port = Number(process.env.PORT || 3101);
 console.log(`Server is running on port ${port}`);

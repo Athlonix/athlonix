65aaea0eb7ec48c86eca9b14126faf0707fce72a
Antony David <antonydavid945@gmail.com>
2024-04-07 18:14:46 +0200

    fix: css not rendering

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@repo/ui/components/button";
+import { Button } from '@repo/ui/components/button';
 
 export default function Page(): JSX.Element {
-    return (
-        <main>
-            <Button>Click me</Button>
-        </main>
-    );
+  return (
+    <main>
+      <Button>Click me</Button>
+    </main>
+  );
 }

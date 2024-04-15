5cb42ae915b550409e1486585bab25e735dcd0e8
Metololo <93775692+Metololo@users.noreply.github.com>
2024-04-15 20:15:08 +0200

    feat(client): add PostFiltering componenet

--- apps/client/app/(withNavbar)/blog/page.tsx
+++ apps/client/app/(withNavbar)/blog/page.tsx
@@ -1,12 +1,14 @@
 import { navLinks } from '@/app/lib/navlinks';
 import { NavBar } from '@/app/ui/NavBar';
-import { Button } from '@repo/ui/components/ui/button';
+import { PostFiltering } from '@/app/ui/PostFiltering';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@repo/ui/components/ui/select';
 
 export default function Page(): JSX.Element {
   return (
     <>
       <NavBar links={navLinks} />
       <main className="flex flex-col items-center gap-y-8 py-6">blog</main>
+      <PostFiltering />
     </>
   );
 }
---
+++ apps/client/app/ui/PostFiltering.tsx
@@ -0,0 +1,41 @@
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@repo/ui/components/ui/select';
+import type React from 'react';
+
+export const PostFiltering: React.FC = () => {
+  return (
+    <div className="flex items-center justify-normal gap-6">
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Light" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
+
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
+
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};

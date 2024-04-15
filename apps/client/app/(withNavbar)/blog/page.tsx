24ac3168f9d42eb0add5c393872d3986a30875df
Metololo <93775692+Metololo@users.noreply.github.com>
2024-04-15 19:00:48 +0200

    chore: add '@' import for nextjs apps

---
+++ apps/client/app/(withNavbar)/blog/page.tsx
@@ -0,0 +1,14 @@
+import { navLinks } from '@/app/lib/navlinks';
+import { NavBar } from '@/app/ui/NavBar';
+import { Button } from '@repo/ui/components/ui/button';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Page(): JSX.Element {
+  return (
+    <>
+      <NavBar links={navLinks} />
+      <main className="flex flex-col items-center gap-y-8 py-6">blog</main>
+    </>
+  );
+}

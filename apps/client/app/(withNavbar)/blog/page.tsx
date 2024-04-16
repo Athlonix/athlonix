feeae6a9109471aca886813ffda9bfc6702463bf
Metololo <93775692+Metololo@users.noreply.github.com>
2024-04-16 23:01:48 +0200

    feat: add search bar componenet

--- apps/client/app/(withNavbar)/blog/page.tsx
+++ apps/client/app/(withNavbar)/blog/page.tsx
@@ -1,6 +1,8 @@
 import { navLinks } from '@/app/lib/navlinks';
 import { NavBar } from '@/app/ui/NavBar';
-import { PostFiltering } from '@/app/ui/PostFiltering';
+import { PostFiltering } from '@/app/ui/components/PostFiltering';
+import { SearchBar } from '@/app/ui/components/SearchBar';
+import { AutoComplete } from '@repo/ui/components/ui/AutoComplete';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@repo/ui/components/ui/select';
 
 export default function Page(): JSX.Element {
@@ -9,6 +11,7 @@ export default function Page(): JSX.Element {
       <NavBar links={navLinks} />
       <main className="flex flex-col items-center gap-y-8 py-6">blog</main>
       <PostFiltering />
+      <SearchBar />
     </>
   );
 }

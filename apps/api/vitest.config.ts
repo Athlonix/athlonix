28379dbf9ce92e2f72b395cba62b4c1a8aa752a0
Antony David <antonydavid945@gmail.com>
2024-04-18 21:00:48 +0200

    fix: supa in ci

--- apps/api/vitest.config.ts
+++ apps/api/vitest.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
-    maxConcurrency: 1,
+    dangerouslyIgnoreUnhandledErrors: true,
   },
 });

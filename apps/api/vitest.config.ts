55fc0842c497c77f08ca929a7250b64f9e73f128
Antony David <antonydavid945@gmail.com>
2024-04-18 20:40:30 +0200

    ci: fix selhosted

--- apps/api/vitest.config.ts
+++ apps/api/vitest.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
-    dangerouslyIgnoreUnhandledErrors: true,
+    maxConcurrency: 1,
   },
 });

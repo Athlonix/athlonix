1dcc47d55e61251936c210b662a96252c4d953c4
MetooSHEESH <93775692+Metololo@users.noreply.github.com>
2024-04-25 13:55:02 +0200

    refactor: env variables name for nextjs

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -3,12 +3,12 @@ module.exports = {
   transpilePackages: ['@repo/ui'],
   output: 'standalone',
   images: {
-    domains: [process.env.STORAGE_DOMAIN],
+    domains: [process.env.SUPABASE_DOMAIN],
     minimumCacheTTL: 60,
     formats: ['image/avif', 'image/webp'],
   },
   env: {
-    ATHLONIX_API_URL: process.env.ATHLONIX_API_URL,
-    ATHLONIX_STORAGE_URL: process.env.ATHLONIX_STORAGE_URL,
+    ATHLONIX_API_URL: process.env.NEXT_PUBLIC_ATHLONIX_API_URL,
+    ATHLONIX_STORAGE_URL: process.env.NEXT_PUBLIC_ATHLONIX_STORAGE_URL,
   },
 };

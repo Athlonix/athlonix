8dd1a850f531a286b679e0b169000baa0ff96c9c
Simon ZHANG <102216594+userMeh@users.noreply.github.com>
2024-04-25 09:27:20 +0200

    fix: wrap useSearchParams in Suspense

--- apps/admin/app/(dashboard)/dashboard/users/page.tsx
+++ apps/admin/app/(dashboard)/dashboard/users/page.tsx
@@ -25,7 +25,7 @@ import { Tabs, TabsContent } from '@repo/ui/components/ui/tabs';
 import { Toaster } from '@repo/ui/components/ui/toaster';
 import { ListFilter } from 'lucide-react';
 import { useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 
 type User = {
   id: number;
@@ -38,12 +38,13 @@ type User = {
   roles: { id: number; name: string }[];
 };
 
-export default function Page(): JSX.Element {
+function ShowContent() {
   const searchParams = useSearchParams();
   let page = searchParams.get('page') || 1;
   if (typeof page === 'string') {
     page = Number.parseInt(page);
   }
+
   const [maxPage, setMaxPage] = useState<number>(1);
   const [users, setUsers] = useState<User[]>([]);
 
@@ -85,8 +86,107 @@ export default function Page(): JSX.Element {
       });
   }, [page]);
 
-  console.log(maxPage, page);
+  return (
+    <TabsContent value="all">
+      <Card x-chunk="dashboard-06-chunk-0">
+        <CardHeader className="flex flex-row items-center justify-between">
+          <CardTitle>Utilisateurs</CardTitle>
+          <div className="flex gap-4">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline" size="sm" className="h-8 gap-1">
+                  <ListFilter className="h-3.5 w-3.5" />
+                  <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">Rôles</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuCheckboxItem>Retirer</DropdownMenuCheckboxItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuCheckboxItem>Banni</DropdownMenuCheckboxItem>
+                <DropdownMenuCheckboxItem checked>Utilisateur</DropdownMenuCheckboxItem>
+                <DropdownMenuCheckboxItem>Rédacteur</DropdownMenuCheckboxItem>
+                <DropdownMenuCheckboxItem>Modérateur</DropdownMenuCheckboxItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <AddUser users={users} />
+          </div>
+        </CardHeader>
+        <CardContent>
+          <div className="ml-auto flex items-center gap-2">
+            <Input
+              type="search"
+              placeholder="Search..."
+              className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
+            />
+          </div>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Nom d'utilisateur</TableHead>
+                <TableHead>Prénom</TableHead>
+                <TableHead>Nom</TableHead>
+                <TableHead>Email</TableHead>
+                <TableHead>Référant</TableHead>
+                <TableHead>Rôle</TableHead>
+                <TableHead>Date d'éxpiration</TableHead>
+                <TableHead>Date de création</TableHead>
+                <TableHead>
+                  <span className="sr-only">Actions</span>
+                </TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <UsersList users={users} />
+            </TableBody>
+          </Table>
+        </CardContent>
+        <CardFooter>
+          <Pagination>
+            <PaginationContent>
+              {page > 1 && (
+                <PaginationItem>
+                  <PaginationPrevious href={`/dashboard/users?page=${page - 1}`} />
+                </PaginationItem>
+              )}
+              {page > 3 && (
+                <PaginationItem>
+                  <PaginationLink href={`/dashboard/users?page=${page - 2}`}>{page - 2}</PaginationLink>
+                </PaginationItem>
+              )}
+              {page > 2 && (
+                <PaginationItem>
+                  <PaginationLink href={`/dashboard/users?page=${page - 1}`}>{page - 1}</PaginationLink>
+                </PaginationItem>
+              )}
+              <PaginationItem>
+                <PaginationLink href={`/dashboard/users?page=${page}`} isActive>
+                  {page}
+                </PaginationLink>
+              </PaginationItem>
+              {page < maxPage && (
+                <PaginationItem>
+                  <PaginationLink href={`/dashboard/users?page=${page + 1}`}>{page + 1}</PaginationLink>
+                </PaginationItem>
+              )}
+              {page < maxPage - 1 && (
+                <PaginationItem>
+                  <PaginationLink href={`/dashboard/users?page=${page + 2}`}>{page + 2}</PaginationLink>
+                </PaginationItem>
+              )}
+              {page < maxPage && (
+                <PaginationItem>
+                  <PaginationNext href={`/dashboard/users?page=${page + 1}`} />
+                </PaginationItem>
+              )}
+            </PaginationContent>
+          </Pagination>
+        </CardFooter>
+      </Card>
+    </TabsContent>
+  );
+}
 
+export default function Page(): JSX.Element {
   return (
     <main>
       <div className="flex flex-col h-full">
@@ -94,102 +194,9 @@ export default function Page(): JSX.Element {
           <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
             <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
               <Tabs defaultValue="all">
-                <TabsContent value="all">
-                  <Card x-chunk="dashboard-06-chunk-0">
-                    <CardHeader className="flex flex-row items-center justify-between">
-                      <CardTitle>Utilisateurs</CardTitle>
-                      <div className="flex gap-4">
-                        <DropdownMenu>
-                          <DropdownMenuTrigger asChild>
-                            <Button variant="outline" size="sm" className="h-8 gap-1">
-                              <ListFilter className="h-3.5 w-3.5" />
-                              <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">Rôles</span>
-                            </Button>
-                          </DropdownMenuTrigger>
-                          <DropdownMenuContent align="end">
-                            <DropdownMenuCheckboxItem>Retirer</DropdownMenuCheckboxItem>
-                            <DropdownMenuSeparator />
-                            <DropdownMenuCheckboxItem>Banni</DropdownMenuCheckboxItem>
-                            <DropdownMenuCheckboxItem checked>Utilisateur</DropdownMenuCheckboxItem>
-                            <DropdownMenuCheckboxItem>Rédacteur</DropdownMenuCheckboxItem>
-                            <DropdownMenuCheckboxItem>Modérateur</DropdownMenuCheckboxItem>
-                          </DropdownMenuContent>
-                        </DropdownMenu>
-                        <AddUser users={users} />
-                      </div>
-                    </CardHeader>
-                    <CardContent>
-                      <div className="ml-auto flex items-center gap-2">
-                        <Input
-                          type="search"
-                          placeholder="Search..."
-                          className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
-                        />
-                      </div>
-                      <Table>
-                        <TableHeader>
-                          <TableRow>
-                            <TableHead>Nom d'utilisateur</TableHead>
-                            <TableHead>Prénom</TableHead>
-                            <TableHead>Nom</TableHead>
-                            <TableHead>Email</TableHead>
-                            <TableHead>Référant</TableHead>
-                            <TableHead>Rôle</TableHead>
-                            <TableHead>Date d'éxpiration</TableHead>
-                            <TableHead>Date de création</TableHead>
-                            <TableHead>
-                              <span className="sr-only">Actions</span>
-                            </TableHead>
-                          </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                          <UsersList users={users} />
-                        </TableBody>
-                      </Table>
-                    </CardContent>
-                    <CardFooter>
-                      <Pagination>
-                        <PaginationContent>
-                          {page > 1 && (
-                            <PaginationItem>
-                              <PaginationPrevious href={`/dashboard/users?page=${page - 1}`} />
-                            </PaginationItem>
-                          )}
-                          {page > 3 && (
-                            <PaginationItem>
-                              <PaginationLink href={`/dashboard/users?page=${page - 2}`}>{page - 2}</PaginationLink>
-                            </PaginationItem>
-                          )}
-                          {page > 2 && (
-                            <PaginationItem>
-                              <PaginationLink href={`/dashboard/users?page=${page - 1}`}>{page - 1}</PaginationLink>
-                            </PaginationItem>
-                          )}
-                          <PaginationItem>
-                            <PaginationLink href={`/dashboard/users?page=${page}`} isActive>
-                              {page}
-                            </PaginationLink>
-                          </PaginationItem>
-                          {page < maxPage && (
-                            <PaginationItem>
-                              <PaginationLink href={`/dashboard/users?page=${page + 1}`}>{page + 1}</PaginationLink>
-                            </PaginationItem>
-                          )}
-                          {page < maxPage - 1 && (
-                            <PaginationItem>
-                              <PaginationLink href={`/dashboard/users?page=${page + 2}`}>{page + 2}</PaginationLink>
-                            </PaginationItem>
-                          )}
-                          {page < maxPage && (
-                            <PaginationItem>
-                              <PaginationNext href={`/dashboard/users?page=${page + 1}`} />
-                            </PaginationItem>
-                          )}
-                        </PaginationContent>
-                      </Pagination>
-                    </CardFooter>
-                  </Card>
-                </TabsContent>
+                <Suspense>
+                  <ShowContent />
+                </Suspense>
               </Tabs>
             </main>
           </div>

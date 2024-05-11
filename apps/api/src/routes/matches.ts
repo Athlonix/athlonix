28ad1b5346349d1278ef344514a4fd4007bc204e
Simon ZHANG <102216594+userMeh@users.noreply.github.com>
2024-05-11 14:55:49 +0200

    feat: routes

---
+++ apps/api/src/routes/matches.ts
@@ -0,0 +1,195 @@
+import { createRoute, z } from '@hono/zod-openapi';
+import authMiddleware from '../middlewares/auth.js';
+import { queryAllSchema } from '../utils/pagnination.js';
+import { idParamValidator, notFoundSchema, serverErrorSchema } from '../validators/general.js';
+
+export const getAllMatches = createRoute({
+  method: 'get',
+  path: '/matches',
+  summary: 'Get all matches',
+  description: 'Get all matches',
+  request: {
+    query: queryAllSchema,
+  },
+  responses: {
+    200: {
+      description: 'Successful response',
+      content: {
+        'application/json': {
+          schema: {
+            data: z.array(
+              z.object({
+                id: z.number(),
+                match_start_time: z.string().datetime(),
+                match_end_time: z.string().datetime(),
+                winner: z.array(z.boolean().nullable()),
+              }),
+            ),
+          },
+        },
+      },
+    },
+    500: serverErrorSchema,
+  },
+  tags: ['match'],
+});
+
+export const getMatchById = createRoute({
+  method: 'get',
+  path: '/matches/{id}',
+  summary: 'Get a match',
+  description: 'Get a match',
+  request: {
+    params: idParamValidator,
+  },
+  responses: {
+    200: {
+      description: 'Successful response',
+      content: {
+        'application/json': {
+          schema: {
+            data: z.object({
+              id: z.number(),
+              match_start_time: z.string().datetime(),
+              match_end_time: z.string().datetime(),
+              winner: z.array(z.boolean().nullable()),
+            }),
+          },
+        },
+      },
+    },
+    500: serverErrorSchema,
+    404: notFoundSchema,
+  },
+  tags: ['match'],
+});
+
+export const createMatch = createRoute({
+  method: 'post',
+  path: '/matches',
+  summary: 'Create a match',
+  description: 'Create a match',
+  middleware: authMiddleware,
+  request: {
+    body: {
+      content: {
+        'application/json': {
+          schema: z.object({
+            match_start_time: z.string().datetime(),
+            match_end_time: z.string().datetime(),
+            winner: z.array(z.boolean().nullable()),
+          }),
+        },
+      },
+    },
+  },
+  responses: {
+    200: {
+      description: 'Successful response',
+    },
+    500: serverErrorSchema,
+  },
+  tags: ['match'],
+});
+
+export const createMatchTournament = createRoute({
+  method: 'post',
+  path: '/matches/tournament/{id_tournament}',
+  summary: 'Create a match for a tournament',
+  description: 'Create a match for a tournament',
+  middleware: authMiddleware,
+  request: {
+    params: z.object({
+      id_tournament: z.number().min(1),
+    }),
+    body: {
+      content: {
+        'application/json': {
+          schema: z.object({
+            round: z.number().min(1),
+          }),
+        },
+      },
+    },
+  },
+  responses: {
+    200: {
+      description: 'Successful response',
+    },
+    500: serverErrorSchema,
+  },
+  tags: ['match'],
+});
+
+export const updateMatch = createRoute({
+  method: 'put',
+  path: '/matches/{id}',
+  summary: 'Update a match',
+  description: 'Update a match',
+  middleware: authMiddleware,
+  request: {
+    params: idParamValidator,
+    body: {
+      content: {
+        'application/json': {
+          schema: z.object({
+            match_start_time: z.string().datetime(),
+            match_end_time: z.string().datetime(),
+          }),
+        },
+      },
+    },
+  },
+  responses: {
+    200: {
+      description: 'Successful response',
+    },
+    500: serverErrorSchema,
+    404: notFoundSchema,
+  },
+  tags: ['match'],
+});
+
+export const updateMatchWinner = createRoute({
+  method: 'patch',
+  path: '/matches/{id}/winner',
+  summary: 'Update the winner of a match',
+  description: 'Update the winner of a match',
+  middleware: authMiddleware,
+  request: {
+    params: idParamValidator,
+    request: {
+      query: z.object({
+        idTeam: z.number().min(1),
+        winner: z.boolean(),
+      }),
+    },
+  },
+  responses: {
+    200: {
+      description: 'Successful response',
+    },
+    500: serverErrorSchema,
+    404: notFoundSchema,
+  },
+  tags: ['match'],
+});
+
+export const deleteMatch = createRoute({
+  method: 'delete',
+  path: '/matches/{id}',
+  summary: 'Delete a match',
+  description: 'Delete a match',
+  middleware: authMiddleware,
+  request: {
+    params: idParamValidator,
+  },
+  responses: {
+    200: {
+      description: 'Successful response',
+    },
+    500: serverErrorSchema,
+    404: notFoundSchema,
+  },
+  tags: ['match'],
+});
---
+++ apps/api/src/routes/tournaments.ts
@@ -0,0 +1,378 @@
+import { createRoute, z } from '@hono/zod-openapi';
+import authMiddleware from '../middlewares/auth.js';
+import { queryAllSchema } from '../utils/pagnination.js';
+import { idParamValidator, notFoundSchema, serverErrorSchema } from '../validators/general.js';
+
+export const tournamentSchema = z.object({
+  id: z.number(),
+  default_match_length: z.number().min(1).nullable(),
+  name: z.string().max(255),
+  max_participants: z.number().min(1),
+  team_capacity: z.number().min(1),
+  rules: z.string().nullable(),
+  prize: z.string().nullable(),
+  id_address: z.number().min(1).nullable(),
+  created_at: z.string().datetime(),
+});
+
+export const getAllTournaments = createRoute({
+  method: 'get',
+  path: '/tournaments',
+  summary: 'Get all tournaments',
+  description: 'Get all tournaments',
+  request: {
+    query: z.object({
+      ...queryAllSchema.shape,
+      id_sport: z.number().min(1).optional(),
+      id_address: z.number().min(1).optional(),
+    }),
+  },
+  responses: {
+    200: {
+      description: 'Successful response',
+      content: {
+        'application/json': {
+          schema: {
+            data: z.array(tournamentSchema),
+          },
+        },
+      },
+    },
+    500: serverErrorSchema,
+  },
+  tags: ['tournament'],
+});
+
+export const getTournamentById = createRoute({
+  method: 'get',
+  path: '/tournaments/{id}',
+  summary: 'Get a tournament',
+  description: 'Get a tournament',
+  request: {
+    params: idParamValidator,
+  },
+  responses: {
+    200: {
+      description: 'Successful response',
+      content: {
+        'application/json': {
+          schema: {
+            data: tournamentSchema,
+          },
+        },
+      },
+    },
+    500: serverErrorSchema,
+    404: notFoundSchema,
+  },
+  tags: ['tournament'],
+});
+
+export const createTournament = createRoute({
+  method: 'post',
+  path: '/tournaments',
+  summary: 'Create a tournament',
+  description: 'Create a tournament',
+  security: [{ bearerAuth: [] }],
+  middleware: authMiddleware,
+  request: {
+    body: {
+      content: {
+        'application/json': {
+          schema: z.object({
+            default_match_length: z.number().min(1).optional(),
+            name: z.string().max(255),
+            max_participants: z.number().min(1),
+            team_capacity: z.number().min(1),
+            rules: z.string().optional(),
+            prize: z.string().optional(),
+            id_address: z.number().min(1).optional(),
+          }),
+        },
+      },
+    },
+  },
+  responses: {
+    200: {
+      description: 'Successful response',
+      content: {
+        'application/json': {
+          schema: tournamentSchema,
+        },
+      },
+    },
+    500: serverErrorSchema,
+  },
+  tags: ['tournament'],
+});
+
+export const updateTournament = createRoute({
+  method: 'patch',
+  path: '/tournaments/{id}',
+  summary: 'Update a tournament',
+  description: 'Update a tournament',
+  security: [{ bearerAuth: [] }],
+  middleware: authMiddleware,
+  request: {
+    params: idParamValidator,
+    body: {
+      content: {
+        'application/json': {
+          schema: z.object({
+            default_match_length: z.number().min(1).optional(),
+            name: z.string().max(255).optional(),
+            max_participants: z.number().min(1).optional(),
+            team_capacity: z.number().min(1).optional(),
+            rules: z.string().optional(),
+            prize: z.string().optional(),
+            id_address: z.number().min(1).optional(),
+          }),
+        },
+      },
+    },
+  },
+  responses: {
+    200: {
+      description: 'Successful response',
+      content: {
+        'application/json': {
+          schema: tournamentSchema,
+        },
+      },
+    },
+    500: serverErrorSchema,
+    404: notFoundSchema,
+  },
+  tags: ['tournament'],
+});
+
+export const deleteTournament = createRoute({
+  method: 'delete',
+  path: '/tournaments/{id}',
+  summary: 'Delete a tournament',
+  description: 'Delete a tournament',
+  security: [{ bearerAuth: [] }],
+  middleware: authMiddleware,
+  request: {
+    params: idParamValidator,
+  },
+  responses: {
+    204: {
+      description: 'Successful response',
+    },
+    500: serverErrorSchema,
+    404: notFoundSchema,
+  },
+  tags: ['tournament'],
+});
+
+export const getTournamentTeams = createRoute({
+  method: 'get',
+  path: '/tournaments/{id}/teams',
+  summary: 'Get all teams of a tournament',
+  description: 'Get all teams of a tournament',
+  request: {
+    params: idParamValidator,
+    query: queryAllSchema,
+  },
+  responses: {
+    200: {
+      description: 'Successful response',
+      content: {
+        'application/json': {
+          schema: {
+            data: z.array(
+              z.object({
+                id: z.number(),
+                name: z.string().max(255),
+                created_at: z.string().datetime(),
+              }),
+            ),
+          },
+        },
+      },
+    },
+    500: serverErrorSchema,
+    404: notFoundSchema,
+  },
+  tags: ['tournament'],
+});
+
+export const getTournamentTeamsById = createRoute({
+  method: 'get',
+  path: '/tournaments/{id}/teams/{id_team}',
+  summary: 'Get a team of a tournament',
+  description: 'Get a team of a tournament',
+  request: {
+    params: z.object({
+      id: z.coerce.number(),
+      id_team: z.coerce.number(),
+    }),
+  },
+  responses: {
+    200: {
+      description: 'Successful response',
+      content: {
+        'application/json': {
+          schema: {
+            data: z.object({
+              id: z.number(),
+              name: z.string().max(255),
+              created_at: z.string().datetime(),
+            }),
+          },
+        },
+      },
+    },
+    404: notFoundSchema,
+    500: serverErrorSchema,
+  },
+  tags: ['tournament'],
+});
+
+export const createTeams = createRoute({
+  method: 'post',
+  path: '/tournaments/{id}/teams',
+  summary: 'Create a team for a tournament',
+  description: 'Create a team for a tournament',
+  security: [{ bearerAuth: [] }],
+  middleware: authMiddleware,
+  request: {
+    params: idParamValidator,
+    body: {
+      content: {
+        'application/json': {
+          schema: z.object({
+            name: z.string().max(255),
+          }),
+        },
+      },
+    },
+  },
+  responses: {
+    200: {
+      description: 'Successful response',
+      content: {
+        'application/json': {
+          schema: z.object({
+            id: z.number(),
+            name: z.string().max(255),
+            created_at: z.string().datetime(),
+          }),
+        },
+      },
+    },
+    500: serverErrorSchema,
+  },
+  tags: ['tournament'],
+});
+
+export const updateTeam = createRoute({
+  method: 'patch',
+  path: '/tournaments/{id}/teams/{id_team}',
+  summary: 'Update a team of a tournament',
+  description: 'Update a team of a tournament',
+  security: [{ bearerAuth: [] }],
+  middleware: authMiddleware,
+  request: {
+    params: z.object({
+      id: z.coerce.number(),
+      id_team: z.coerce.number(),
+    }),
+    body: {
+      content: {
+        'application/json': {
+          schema: z.object({
+            name: z.string().max(255).optional(),
+          }),
+        },
+      },
+    },
+  },
+  responses: {
+    200: {
+      description: 'Successful response',
+      content: {
+        'application/json': {
+          schema: z.object({
+            id: z.number(),
+            name: z.string().max(255),
+            created_at: z.string().datetime(),
+          }),
+        },
+      },
+    },
+    500: serverErrorSchema,
+    404: notFoundSchema,
+  },
+  tags: ['tournament'],
+});
+
+export const deleteTeam = createRoute({
+  method: 'delete',
+  path: '/tournaments/{id}/teams/{id_team}',
+  summary: 'Delete a team of a tournament',
+  description: 'Delete a team of a tournament',
+  security: [{ bearerAuth: [] }],
+  middleware: authMiddleware,
+  request: {
+    params: z.object({
+      id: z.coerce.number(),
+      id_team: z.coerce.number(),
+    }),
+  },
+  responses: {
+    204: {
+      description: 'Successful response',
+    },
+    500: serverErrorSchema,
+    404: notFoundSchema,
+  },
+  tags: ['tournament'],
+});
+
+export const joinTeam = createRoute({
+  method: 'post',
+  path: '/tournaments/{id}/teams/{id_team}/join',
+  summary: 'Join a team of a tournament',
+  description: 'Join a team of a tournament',
+  security: [{ bearerAuth: [] }],
+  middleware: authMiddleware,
+  request: {
+    params: z.object({
+      id: z.coerce.number(),
+      id_team: z.coerce.number(),
+    }),
+  },
+  responses: {
+    204: {
+      description: 'Successful response',
+    },
+    500: serverErrorSchema,
+    404: notFoundSchema,
+  },
+  tags: ['tournament'],
+});
+
+export const leaveTeam = createRoute({
+  method: 'post',
+  path: '/tournaments/{id}/teams/{id_team}/leave',
+  summary: 'Leave a team of a tournament',
+  description: 'Leave a team of a tournament',
+  security: [{ bearerAuth: [] }],
+  middleware: authMiddleware,
+  request: {
+    params: z.object({
+      id: z.coerce.number(),
+      id_team: z.coerce.number(),
+    }),
+  },
+  responses: {
+    204: {
+      description: 'Successful response',
+    },
+    500: serverErrorSchema,
+    404: notFoundSchema,
+  },
+  tags: ['tournament'],
+});

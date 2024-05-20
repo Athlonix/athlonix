4514fb1719868c5370f7d9bce04aff59bce5c12d
Antony David <antonydavid945@gmail.com>
2024-05-20 21:11:37 +0200

    feat: listDonations

--- apps/api/src/handlers/stripe.ts
+++ apps/api/src/handlers/stripe.ts
@@ -2,9 +2,11 @@ import { OpenAPIHono } from '@hono/zod-openapi';
 import type { Context } from 'hono';
 import Stripe from 'stripe';
 import { handleDonations } from '../libs/stripe.js';
+import { supabase } from '../libs/supabase.js';
 import { zodErrorHook } from '../libs/zodError.js';
-import { webhook } from '../routes/stripe.js';
+import { listDonations, webhook } from '../routes/stripe.js';
 import { checkRole } from '../utils/context.js';
+import { getPagination } from '../utils/pagnination.js';
 import type { Variables } from '../validators/general.js';
 
 export const stripe = new OpenAPIHono<{ Variables: Variables }>({
@@ -25,7 +27,6 @@ stripe.openapi(webhook, async (context: Context) => {
 
     switch (event.type) {
       case 'charge.succeeded': {
-        console.log(event.data.object);
         const email = event.data.object.receipt_email || 'test@email.com';
         const amount = event.data.object.amount / 100;
         const receipt_url = event.data.object.receipt_url as string;
@@ -45,4 +46,42 @@ stripe.openapi(webhook, async (context: Context) => {
     }`;
     return context.json({ error: errorMessage }, 500);
   }
+});
+
+stripe.openapi(listDonations, async (c) => {
+  const user = c.get('user');
+  const roles = user.roles;
+  await checkRole(roles, false);
+  const { startDate, endDate, skip, take, all, search } = c.req.valid('query');
+  const query = supabase.from('DONATIONS').select('*').order('created_at', { ascending: true });
+
+  if (startDate) {
+    query.gte('created_at', startDate);
+  }
+
+  if (endDate) {
+    query.lte('created_at', endDate);
+  }
+
+  if (search) {
+    const isNumber = !Number.isNaN(Number(search));
+    if (isNumber) {
+      query.eq('amount', Number(search));
+    }
+  }
+
+  if (!all) {
+    const { from, to } = getPagination(skip, take - 1);
+    query.range(from, to);
+  }
+
+  const { data, error, count } = await query.range(skip, take);
+
+  if (error) {
+    return c.json({ error: error.message }, 500);
+  }
+
+  const total = data.reduce((acc, curr) => acc + curr.amount, 0);
+
+  return c.json({ data, total, count: count || 0 }, 200);
 });
--- apps/api/src/routes/stripe.ts
+++ apps/api/src/routes/stripe.ts
@@ -1,7 +1,20 @@
 import { createRoute } from '@hono/zod-openapi';
 import { z } from 'zod';
 import authMiddleware from '../middlewares/auth.js';
-import { badRequestSchema, notFoundSchema, serverErrorSchema } from '../validators/general.js';
+import { queryAllSchema } from '../utils/pagnination.js';
+import { badRequestSchema, serverErrorSchema } from '../validators/general.js';
+
+export const donationSchema = z.object({
+  id: z.number().min(1),
+  amount: z.number().positive(),
+  receipt_url: z.string().url(),
+  id_user: z.number().min(1).nullable(),
+});
+
+const query = queryAllSchema.extend({
+  startDate: z.string().date().optional(),
+  endDate: z.string().date().optional(),
+});
 
 export const webhook = createRoute({
   method: 'post',
@@ -21,6 +34,35 @@ export const webhook = createRoute({
     },
     500: serverErrorSchema,
     400: badRequestSchema,
+  },
+  tags: ['stripe'],
+});
+
+export const listDonations = createRoute({
+  method: 'get',
+  path: '/stripe/donations',
+  summary: 'List donations',
+  description: 'List donations',
+  security: [{ Bearer: [] }],
+  middleware: authMiddleware,
+  request: {
+    query,
+  },
+  responses: {
+    200: {
+      description: 'Successful response',
+      content: {
+        'application/json': {
+          schema: z.object({
+            data: z.array(donationSchema),
+            total: z.number().positive(),
+            count: z.number().positive(),
+          }),
+        },
+      },
+    },
+    400: badRequestSchema,
+    500: serverErrorSchema,
   },
   tags: ['stripe'],
 });

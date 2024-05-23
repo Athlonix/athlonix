2aa168b3732c8df725bc41d179ce1c493c9c2c8a
Antony David <antonydavid945@gmail.com>
2024-05-23 13:20:24 +0200

    fix: roles checking workflow (#136)
    * fix: roles checking workflow
    
    * fix: all test
    
    * fix: add missing migration for subscription
    
    * fix: user tests

--- apps/api/src/middlewares/auth.ts
+++ apps/api/src/middlewares/auth.ts
@@ -15,12 +15,10 @@ const authMiddleware: MiddlewareHandler = async (c, next) => {
       .single();
 
     if (userError || !user) throw new HTTPException(404, { message: 'User not found' });
-    if (user.date_validity !== null && new Date(user.date_validity) < new Date())
-      throw new HTTPException(403, { message: 'User validity expired' });
 
     c.set('user', {
       id: user.id,
-      roles: user.USERS_ROLES.map((role) => role.id_role),
+      roles: accountRolesValidity(user.date_validity, user.USERS_ROLES),
       email: data.user?.email,
       updated_at: data.user?.updated_at,
       created_at: data.user?.created_at,
@@ -31,5 +29,21 @@ const authMiddleware: MiddlewareHandler = async (c, next) => {
 
   await next();
 };
+
+/* Info :
+if date is null and roles is empty it means that it's a basic user with no roles.
+
+if date is not null and it's less than the current date it means that the user subscription is expired, 
+so it has no roles but we don't remove user roles from the database.
+
+if date is not null and it's greater than the current date it means that the user subscription is still valid.
+*/
+function accountRolesValidity(date: null | string, roles: { id_role: number }[]): number[] {
+  if (date === null) return [];
+
+  if (date !== null && new Date(date) < new Date()) return [];
+
+  return roles.map((role) => role.id_role);
+}
 
 export default authMiddleware;
--- apps/api/test/tournaments.test.ts
+++ apps/api/test/tournaments.test.ts
@@ -1,6 +1,6 @@
 import app from '../src/index.js';
 import { Role } from '../src/validators/general.js';
-import { deleteAdmin, insertRole } from './utils.js';
+import { deleteAdmin, insertRole, setValidSubscription } from './utils.js';
 
 const port = Number(process.env.PORT || 3101);
 const path = `http://localhost:${port}`;
@@ -30,6 +30,7 @@ describe('Tournaments tests', () => {
     id_user = user.id;
     await insertRole(id_user, Role.ADMIN);
     await insertRole(id_user, Role.MEMBER);
+    await setValidSubscription(id_user);
 
     const loginRes = await app.request(`${path}/auth/login`, {
       method: 'POST',

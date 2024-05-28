6e46bf318b9f265801e4b5fdbeb78aac6b41f495
Metololo <93775692+Metololo@users.noreply.github.com>
2024-05-28 21:22:27 +0200

    feat: add activities tasks routes (#145)
    * feat: add activity table and employee role
    
    * feat: add route to add employee in activity team
    
    * feat: add activity team route
    
    * feat: get one activity team
    
    * Merge branch 'main' into admin/feat/tournaments
    
    * feat: modify activities database schema
    
    * feat: modify activities schema
    
    * fix: activities types
    
    * fix: pnpm-lock
    
    * feat: enhance activity routes
    
    * fix: activities test
    
    * fix: update migrations
    
    * fix: lint project
    
    * feat: add activity occurence route and tasks
    
    * feat: add tasks route for insertion and get all
    
    * feat: enhance task creation and add get one task route
    
    * fix: update pnpm
    
    * feat: add tests for activities and teams
    
    * fix: lint for pull request
    
    * fix: spelling error
    
    * feat: add delete task
    
    * feat: add tasks patch route
    
    * feat: add tasks tests

--- apps/api/src/routes/tasks.ts
+++ apps/api/src/routes/tasks.ts
@@ -1,13 +1,13 @@
 import { createRoute, z } from '@hono/zod-openapi';
 import authMiddleware from '../middlewares/auth.js';
 import { queryAllSchema } from '../utils/pagnination.js';
-import { badRequestSchema, notFoundSchema, serverErrorSchema } from '../validators/general.js';
+import { badRequestSchema, idParamValidator, notFoundSchema, serverErrorSchema } from '../validators/general.js';
 import { taskSchema } from '../validators/tasks.js';
 
 export const getAllTasks = createRoute({
   method: 'get',
   path: '/activities_exceptions/{id}/tasks',
-  summary: 'Get all activity occurence tasks',
+  summary: 'Get all tasks of an activity occurence',
   description: 'Get all activity occurence tasks',
   request: {
     params: z.object({ id: z.coerce.number().min(1) }),
@@ -83,6 +83,66 @@ export const createTask = createRoute({
     400: badRequestSchema,
     404: notFoundSchema,
     500: serverErrorSchema,
+  },
+  tags: ['task'],
+});
+
+export const deleteTask = createRoute({
+  method: 'delete',
+  path: '/tasks/{id}',
+  summary: 'Delete a task',
+  description: 'Delete a task',
+  security: [{ Bearer: [] }],
+  middleware: authMiddleware,
+  request: {
+    params: z.object({ id: z.coerce.number().min(1) }),
+  },
+  responses: {
+    200: {
+      description: 'Successful response',
+      content: {
+        'application/json': {
+          schema: z.object({
+            message: z.string(),
+          }),
+        },
+      },
+    },
+    500: serverErrorSchema,
+    404: notFoundSchema,
+  },
+  tags: ['task'],
+});
+
+export const updateTask = createRoute({
+  method: 'patch',
+  path: '/tasks/{id}',
+  summary: 'Update a task',
+  description: 'Update a task',
+  security: [{ Bearer: [] }],
+  middleware: authMiddleware,
+  request: {
+    params: idParamValidator,
+    body: {
+      content: {
+        'application/json': {
+          schema: taskSchema.omit({ id: true, created_at: true, id_activity_exception: true }).partial(),
+        },
+      },
+    },
+  },
+  responses: {
+    200: {
+      description: 'Successful response',
+      content: {
+        'application/json': {
+          schema: taskSchema,
+        },
+      },
+    },
+    500: serverErrorSchema,
+    400: badRequestSchema,
+    404: notFoundSchema,
   },
   tags: ['task'],
 });
---
+++ apps/api/test/tasks.test.ts
@@ -0,0 +1,311 @@
+import { startTime } from 'hono/timing';
+import app from '../src/index.js';
+import { Role } from '../src/validators/general.js';
+import { deleteAdmin, insertRole, setValidSubscription } from './utils.js';
+
+const port = Number(process.env.PORT || 3101);
+const path = `http://localhost:${port}`;
+
+describe('Activities tests', () => {
+  let id_admin: number;
+  let id_auth: string;
+  let id_user: number;
+  let jwt: string;
+  let activity_id: number;
+  let id_sport: number;
+  let id_location: number;
+  let id_activity_exception: number;
+  let id_task: number;
+
+  test('Create admin', async () => {
+    const res = await app.request(`${path}/auth/signup`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        first_name: 'admin_activity',
+        last_name: 'admin_activity',
+        username: 'admin_activity',
+        email: 'admin_activity@gmail.com',
+        password: 'password123456',
+      }),
+    });
+    expect(res.status).toBe(201);
+    const user: { id: number; id_auth: string } = await res.json();
+    id_auth = user.id_auth;
+    id_admin = user.id;
+    await insertRole(id_admin, Role.ADMIN);
+    await insertRole(id_admin, Role.MEMBER);
+    await setValidSubscription(id_admin);
+  });
+
+  test('Login admin', async () => {
+    const res = await app.request(`${path}/auth/login`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        email: 'admin_activity@gmail.com',
+        password: 'password123456',
+      }),
+    });
+    expect(res.status).toBe(200);
+    const admin: { token: string } = await res.json();
+    jwt = admin.token;
+  });
+
+  test('Create sport', async () => {
+    const res = await app.request(`${path}/sports`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${jwt}`,
+      },
+      body: JSON.stringify({
+        name: 'Sport activities test',
+        description: 'Description test',
+        min_players: 1,
+        max_players: 10,
+        image: 'image',
+      }),
+    });
+    expect(res.status).toBe(201);
+    const sport: { id: number } = await res.json();
+    id_sport = sport.id;
+  });
+
+  test('Create location', async () => {
+    const res = await app.request(`${path}/addresses`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${jwt}`,
+      },
+      body: JSON.stringify({
+        road: 'string',
+        postal_code: 'string',
+        complement: 'string',
+        city: 'string',
+        number: 0,
+        name: 'string',
+        id_lease: null,
+      }),
+    });
+    expect(res.status).toBe(201);
+    const location: { id: number } = await res.json();
+    id_location = location.id;
+  });
+
+  test('Create activity', async () => {
+    const now = new Date();
+    const end = new Date(new Date().setFullYear(new Date().getFullYear() + 1));
+
+    const start_time = now.toTimeString().split(' ')[0];
+    const end_time = end.toTimeString().split(' ')[0];
+
+    const res = await app.request(`${path}/activities`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${jwt}`,
+      },
+      body: JSON.stringify({
+        name: 'Activity test',
+        description: 'Description test',
+        max_participants: 10,
+        min_participants: 1,
+        days_of_week: ['monday', 'tuesday'],
+        start_date: null,
+        end_date: null,
+        start_time: start_time,
+        end_time: end_time,
+        frequency: 'weekly',
+        id_sport: id_sport,
+        id_address: id_location,
+      }),
+    });
+
+    expect(res.status).toBe(201);
+    const activity: { id: number } = await res.json();
+    activity_id = activity.id;
+  });
+
+  test('Create activity exception', async () => {
+    const res = await app.request(`${path}/activities/${activity_id}/exceptions`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${jwt}`,
+      },
+      body: JSON.stringify({
+        date: '2024-05-27',
+        min_participants: 1,
+        max_participants: 10,
+      }),
+    });
+
+    expect(res.status).toBe(201);
+    const activity_exception: { id: number } = await res.json();
+    id_activity_exception = activity_exception.id;
+  });
+
+  test('Create employee', async () => {
+    const res = await app.request(`${path}/auth/signup`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        first_name: 'user_test',
+        last_name: 'user_test',
+        username: 'user_test',
+        email: 'user_test@gmail.com',
+        password: 'password1234567',
+      }),
+    });
+    expect(res.status).toBe(201);
+    const user: { id: number } = await res.json();
+    id_user = user.id;
+
+    await insertRole(id_user, Role.EMPLOYEE);
+    await setValidSubscription(id_user);
+  });
+
+  test('Add employee to activity team', async () => {
+    const res = await app.request(`${path}/activities/${activity_id}/team/${id_user}`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${jwt}` },
+      body: null,
+    });
+
+    expect(res.status).toBe(201);
+  });
+
+  test('Create task', async () => {
+    const res = await app.request(`${path}/activities_exceptions/${id_activity_exception}/tasks`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${jwt}` },
+      body: JSON.stringify({
+        priority: 'P0',
+        status: 'not started',
+        title: 'Test Activity Task',
+        description: 'Test Activity Task Description',
+        id_employee: id_user,
+      }),
+    });
+    expect(res.status).toBe(201);
+    const task: { id: number } = await res.json();
+    id_task = task.id;
+  });
+
+  test('Get all tasks of exception', async () => {
+    const res = await app.request(`${path}/activities_exceptions/${id_activity_exception}/tasks`, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    });
+    expect(res.status).toBe(200);
+
+    const { data } = await res.json();
+
+    expect(data.length).toBeGreaterThan(0);
+  });
+
+  test('Get single task', async () => {
+    const res = await app.request(`${path}/tasks/${id_task}`, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    });
+    expect(res.status).toBe(200);
+  });
+
+  test('Update task', async () => {
+    const res = await app.request(`${path}/tasks/${id_task}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${jwt}`,
+      },
+      body: JSON.stringify({
+        status: 'completed',
+      }),
+    });
+    expect(res.status).toBe(200);
+  });
+
+  test('Delete task', async () => {
+    const res = await app.request(`${path}/tasks/${id_task}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${jwt}`,
+      },
+    });
+    expect(res.status).toBe(200);
+  });
+
+  test('Remove employee from team', async () => {
+    const res = await app.request(`${path}/activities/${activity_id}/team/${id_user}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${jwt}`,
+      },
+    });
+    expect(res.status).toBe(200);
+  });
+
+  test('Delete activity exception', async () => {
+    const res = await app.request(`${path}/activities_exceptions/${id_activity_exception}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${jwt}`,
+      },
+    });
+    expect(res.status).toBe(200);
+  });
+
+  test('Delete activity', async () => {
+    const res = await app.request(`${path}/activities/${activity_id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${jwt}`,
+      },
+    });
+    expect(res.status).toBe(200);
+  });
+
+  test('Delete user', async () => {
+    const res = await app.request(`${path}/users/${id_user}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${jwt}`,
+      },
+    });
+    expect(res.status).toBe(200);
+  });
+
+  test('Delete location', async () => {
+    const res = await app.request(`${path}/addresses/${id_location}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${jwt}`,
+      },
+    });
+    expect(res.status).toBe(200);
+  });
+
+  test('Delete sport', async () => {
+    const res = await app.request(`${path}/sports/${id_sport}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${jwt}`,
+      },
+    });
+    expect(res.status).toBe(200);
+  });
+
+  afterAll(async () => {
+    await deleteAdmin(id_admin, id_auth);
+  });
+});

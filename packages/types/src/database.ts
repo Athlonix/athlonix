8f802ace1475797e3e71ad54b0e4fa42bc1e1214
Simon <szhang0709@gmail.com>
2024-05-31 14:08:18 +0200

    feat: generate type

--- packages/types/src/database.ts
+++ packages/types/src/database.ts
@@ -30,49 +30,46 @@ export type Database = {
     Tables: {
       ACTIVITIES: {
         Row: {
-          days_of_week: Database['public']['Enums']['days'][] | null;
+          days: Database['public']['Enums']['days'][] | null;
           description: string | null;
-          end_date: string | null;
-          end_time: string | null;
-          frequency: Database['public']['Enums']['frequency'] | null;
+          end_date: string;
           id: number;
           id_address: number | null;
           id_sport: number | null;
+          interval: number;
           max_participants: number;
           min_participants: number;
           name: string;
-          start_date: string | null;
-          start_time: string | null;
+          recurrence: Database['public']['Enums']['recurrence'];
+          start_date: string;
         };
         Insert: {
-          days_of_week?: Database['public']['Enums']['days'][] | null;
+          days?: Database['public']['Enums']['days'][] | null;
           description?: string | null;
-          end_date?: string | null;
-          end_time?: string | null;
-          frequency?: Database['public']['Enums']['frequency'] | null;
+          end_date: string;
           id?: number;
           id_address?: number | null;
           id_sport?: number | null;
+          interval?: number;
           max_participants: number;
           min_participants: number;
           name: string;
-          start_date?: string | null;
-          start_time?: string | null;
+          recurrence: Database['public']['Enums']['recurrence'];
+          start_date: string;
         };
         Update: {
-          days_of_week?: Database['public']['Enums']['days'][] | null;
+          days?: Database['public']['Enums']['days'][] | null;
           description?: string | null;
-          end_date?: string | null;
-          end_time?: string | null;
-          frequency?: Database['public']['Enums']['frequency'] | null;
+          end_date?: string;
           id?: number;
           id_address?: number | null;
           id_sport?: number | null;
+          interval?: number;
           max_participants?: number;
           min_participants?: number;
           name?: string;
-          start_date?: string | null;
-          start_time?: string | null;
+          recurrence?: Database['public']['Enums']['recurrence'];
+          start_date?: string;
         };
         Relationships: [
           {
@@ -121,96 +118,6 @@ export type Database = {
           },
         ];
       };
-      ACTIVITIES_EXCEPTIONS: {
-        Row: {
-          date: string;
-          id: number;
-          id_activity: number;
-          max_participants: number | null;
-          min_participants: number | null;
-        };
-        Insert: {
-          date: string;
-          id?: number;
-          id_activity: number;
-          max_participants?: number | null;
-          min_participants?: number | null;
-        };
-        Update: {
-          date?: string;
-          id?: number;
-          id_activity?: number;
-          max_participants?: number | null;
-          min_participants?: number | null;
-        };
-        Relationships: [
-          {
-            foreignKeyName: 'ACTIVITIES_EXCEPTIONS_id_activity_fkey';
-            columns: ['id_activity'];
-            isOneToOne: false;
-            referencedRelation: 'ACTIVITIES';
-            referencedColumns: ['id'];
-          },
-        ];
-      };
-      ACTIVITIES_TASKS: {
-        Row: {
-          created_at: string;
-          description: string | null;
-          id: number;
-          id_activity: number | null;
-          id_activity_exception: number;
-          id_employee: number | null;
-          priority: Database['public']['Enums']['priority'];
-          status: Database['public']['Enums']['status'];
-          title: string;
-        };
-        Insert: {
-          created_at?: string;
-          description?: string | null;
-          id?: number;
-          id_activity?: number | null;
-          id_activity_exception: number;
-          id_employee?: number | null;
-          priority: Database['public']['Enums']['priority'];
-          status: Database['public']['Enums']['status'];
-          title: string;
-        };
-        Update: {
-          created_at?: string;
-          description?: string | null;
-          id?: number;
-          id_activity?: number | null;
-          id_activity_exception?: number;
-          id_employee?: number | null;
-          priority?: Database['public']['Enums']['priority'];
-          status?: Database['public']['Enums']['status'];
-          title?: string;
-        };
-        Relationships: [
-          {
-            foreignKeyName: 'ACTIVITIES_TASKS_id_activity_fkey';
-            columns: ['id_activity_exception'];
-            isOneToOne: false;
-            referencedRelation: 'ACTIVITIES_EXCEPTIONS';
-            referencedColumns: ['id'];
-          },
-          {
-            foreignKeyName: 'ACTIVITIES_TASKS_id_activity_fkey1';
-            columns: ['id_activity'];
-            isOneToOne: false;
-            referencedRelation: 'ACTIVITIES';
-            referencedColumns: ['id'];
-          },
-          {
-            foreignKeyName: 'ACTIVITIES_TASKS_id_employee_fkey';
-            columns: ['id_employee'];
-            isOneToOne: false;
-            referencedRelation: 'USERS';
-            referencedColumns: ['id'];
-          },
-        ];
-      };
       ACTIVITIES_USERS: {
         Row: {
           active: boolean;
@@ -240,36 +147,6 @@ export type Database = {
           },
           {
             foreignKeyName: 'public_ACTIVITIES_USERS_id_user_fkey';
-            columns: ['id_user'];
-            isOneToOne: false;
-            referencedRelation: 'USERS';
-            referencedColumns: ['id'];
-          },
-        ];
-      };
-      ACTIVITY_TEAMS: {
-        Row: {
-          id_activity: number;
-          id_user: number;
-        };
-        Insert: {
-          id_activity: number;
-          id_user: number;
-        };
-        Update: {
-          id_activity?: number;
-          id_user?: number;
-        };
-        Relationships: [
-          {
-            foreignKeyName: 'ACTIVITY_TEAM_id_activity_fkey';
-            columns: ['id_activity'];
-            isOneToOne: false;
-            referencedRelation: 'ACTIVITIES';
-            referencedColumns: ['id'];
-          },
-          {
-            foreignKeyName: 'ACTIVITY_TEAM_id_user_fkey';
             columns: ['id_user'];
             isOneToOne: false;
             referencedRelation: 'USERS';
@@ -383,74 +260,6 @@ export type Database = {
           {
             foreignKeyName: 'public_APPLICATIONS_id_user_fkey';
             columns: ['id_user'];
-            isOneToOne: false;
-            referencedRelation: 'USERS';
-            referencedColumns: ['id'];
-          },
-        ];
-      };
-      ASSEMBLIES: {
-        Row: {
-          date: string;
-          description: string | null;
-          id: number;
-          lawsuit: string | null;
-          location: number | null;
-          name: string;
-        };
-        Insert: {
-          date: string;
-          description?: string | null;
-          id?: number;
-          lawsuit?: string | null;
-          location?: number | null;
-          name: string;
-        };
-        Update: {
-          date?: string;
-          description?: string | null;
-          id?: number;
-          lawsuit?: string | null;
-          location?: number | null;
-          name?: string;
-        };
-        Relationships: [
-          {
-            foreignKeyName: 'ASSEMBLIES_location_fkey';
-            columns: ['location'];
-            isOneToOne: false;
-            referencedRelation: 'ADDRESSES';
-            referencedColumns: ['id'];
-          },
-        ];
-      };
-      ASSEMBLIES_ATTENDEES: {
-        Row: {
-          id: number;
-          id_assembly: number | null;
-          id_member: number | null;
-        };
-        Insert: {
-          id?: number;
-          id_assembly?: number | null;
-          id_member?: number | null;
-        };
-        Update: {
-          id?: number;
-          id_assembly?: number | null;
-          id_member?: number | null;
-        };
-        Relationships: [
-          {
-            foreignKeyName: 'ASSEMBLIES_ATTENDEES_id_assembly_fkey';
-            columns: ['id_assembly'];
-            isOneToOne: false;
-            referencedRelation: 'ASSEMBLIES';
-            referencedColumns: ['id'];
-          },
-          {
-            foreignKeyName: 'ASSEMBLIES_ATTENDEES_id_member_fkey';
-            columns: ['id_member'];
             isOneToOne: false;
             referencedRelation: 'USERS';
             referencedColumns: ['id'];
@@ -723,17 +532,14 @@ export type Database = {
         Row: {
           id: number;
           name: string;
-          weight_grams: number | null;
         };
         Insert: {
           id?: number;
           name: string;
-          weight_grams?: number | null;
         };
         Update: {
           id?: number;
           name?: string;
-          weight_grams?: number | null;
         };
         Relationships: [];
       };
@@ -816,7 +622,6 @@ export type Database = {
       };
       POLLS: {
         Row: {
-          assembly: number | null;
           description: string | null;
           end_at: string;
           id: number;
@@ -826,7 +631,6 @@ export type Database = {
           title: string;
         };
         Insert: {
-          assembly?: number | null;
           description?: string | null;
           end_at: string;
           id?: number;
@@ -836,7 +640,6 @@ export type Database = {
           title: string;
         };
         Update: {
-          assembly?: number | null;
           description?: string | null;
           end_at?: string;
           id?: number;
@@ -1437,9 +1240,7 @@ export type Database = {
           id: number;
           id_auth: string | null;
           id_referer: number | null;
-          invoice: string | null;
           last_name: string;
-          subscription: string | null;
           username: string;
         };
         Insert: {
@@ -1451,9 +1252,7 @@ export type Database = {
           id?: number;
           id_auth?: string | null;
           id_referer?: number | null;
-          invoice?: string | null;
           last_name: string;
-          subscription?: string | null;
           username: string;
         };
         Update: {
@@ -1465,9 +1264,7 @@ export type Database = {
           id?: number;
           id_auth?: string | null;
           id_referer?: number | null;
-          invoice?: string | null;
           last_name?: string;
-          subscription?: string | null;
           username?: string;
         };
         Relationships: [
@@ -1588,10 +1385,8 @@ export type Database = {
     };
     Enums: {
       days: 'monday' | 'tuesday' | 'wednesday' | 'thursday' | 'friday' | 'saturday' | 'sunday';
-      frequency: 'weekly' | 'monthly' | 'yearly' | 'daily';
-      priority: 'P0' | 'P1' | 'P2' | 'P3';
       reaction: 'like';
-      status: 'not started' | 'in progress' | 'completed';
+      recurrence: 'weekly' | 'monthly' | 'annual';
     };
     CompositeTypes: {
       [_ in never]: never;

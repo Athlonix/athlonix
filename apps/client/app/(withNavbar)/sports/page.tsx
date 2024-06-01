c8e446fbf48900f4c4561279bafea50b91c519bc
Antony David <antonydavid945@gmail.com>
2024-06-01 20:22:56 +0200

    fix(front): wrong server side api url

--- apps/client/app/(withNavbar)/sports/page.tsx
+++ apps/client/app/(withNavbar)/sports/page.tsx
@@ -19,7 +19,10 @@ type SportData = {
 export const dynamic = 'force-dynamic';
 
 async function getSports(): Promise<SportData> {
-  const api_url = process.env.API_URL;
+  const api_url = process.env.ATHLONIX_API_URL;
+  if (!api_url) {
+    throw new Error('ATHLONIX_API_URL is not defined');
+  }
   const response = await fetch(`${api_url}/sports?all=true`, { cache: 'no-cache' });
   if (!response.ok) {
     throw new Error('Erreur lors de la récupération des sports');

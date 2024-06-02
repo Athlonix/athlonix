883f19c79f5a47828a2cda9459930334d288cefc
Antony David <antonydavid945@gmail.com>
2024-06-02 18:30:24 +0200

    fix: subscriptions handler (#158)

--- apps/api/src/handlers/stripe.ts
+++ apps/api/src/handlers/stripe.ts
@@ -1,11 +1,6 @@
 import { OpenAPIHono } from '@hono/zod-openapi';
 import Stripe from 'stripe';
-import {
-  handleDonations,
-  handleRenewalSubscription,
-  handleRevokeSubscription,
-  handleSubscription,
-} from '../libs/stripe.js';
+import { handleDonations, handleRevokeSubscription, handleSubscription } from '../libs/stripe.js';
 import { supabase } from '../libs/supabase.js';
 import { zodErrorHook } from '../libs/zodError.js';
 import { listDonations, webhook } from '../routes/stripe.js';
@@ -18,83 +13,61 @@ export const stripe = new OpenAPIHono<{ Variables: Variables }>({
 });
 
 stripe.openapi(webhook, async (context) => {
-  const STRIPE_SECRET_API_KEY = process.env.STRIPE_SECRET_API_KEY as string;
+  const STRIPE_API_KEY = process.env.STRIPE_API_KEY as string;
   const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET as string;
-  const stripe = new Stripe(STRIPE_SECRET_API_KEY);
+  const stripe = new Stripe(STRIPE_API_KEY);
   const signature = context.req.header('stripe-signature');
-  try {
-    if (!signature) {
-      return context.json({ error: 'No signature' }, 400);
-    }
-    const payload = await context.req.text();
-    const event = await stripe.webhooks.constructEventAsync(payload, signature, STRIPE_WEBHOOK_SECRET);
-
-    switch (event.type) {
-      case 'charge.succeeded': {
-        const email = event.data.object.receipt_email || 'test@email.com';
-        const amount = event.data.object.amount / 100;
-        const receipt_url = event.data.object.receipt_url as string;
-        const data = await handleDonations(email, amount, receipt_url);
-        if (data.error) {
-          return context.json({ error: data.error }, 400);
-        }
-        break;
-      }
-
-      case 'customer.subscription.created': {
-        const subscription = event.data.object.id as string;
-        const invoice = await stripe.invoices.retrieve(event.data.object.latest_invoice as string);
-
-        const data = await handleSubscription(subscription, invoice.invoice_pdf as string);
-        if (data.error) {
-          return context.json({ error: data.error }, 400);
-        }
-        break;
-      }
-      case 'customer.subscription.updated': {
-        const subscription = event.data.object;
-        const previousAttributes = event.data.previous_attributes;
-
-        if (subscription.current_period_end !== previousAttributes?.current_period_end) {
-          const customer = (await stripe.customers.retrieve(subscription.customer as string)) as Stripe.Customer;
-          if (!customer) {
-            return context.json({ error: 'Customer not found' }, 400);
-          }
-
-          if (subscription.status === 'active') {
-            const data = await handleRenewalSubscription(
-              customer,
-              subscription.id,
-              new Date(subscription.current_period_end * 1000),
-            );
-            if (data.error) {
-              return context.json({ error: data.error }, 400);
-            }
-          }
-        }
-
-        break;
-      }
-      case 'customer.subscription.deleted' || 'customer.subscription.paused': {
-        const subscription = event.data.object.id as string;
-        const user = await stripe.subscriptions.retrieve(subscription);
-
-        const data = await handleRevokeSubscription(user.customer as Stripe.Customer);
-        if (data.error) {
-          return context.json({ error: data.error }, 400);
-        }
-        break;
-      }
-      default:
-        break;
-    }
-    return context.json({ message: 'Received' }, 200);
-  } catch (err) {
-    const errorMessage = `⚠️  Webhook signature verification failed. ${
-      err instanceof Error ? err.message : 'Internal server error'
-    }`;
-    return context.json({ error: errorMessage }, 500);
+  if (!signature) {
+    return context.json({ error: 'No signature' }, 400);
   }
+  const payload = await context.req.text();
+  const event = await stripe.webhooks.constructEventAsync(payload, signature, STRIPE_WEBHOOK_SECRET);
+
+  switch (event.type) {
+    case 'charge.succeeded': {
+      const email = event.data.object.receipt_email || 'test@email.com';
+      const amount = event.data.object.amount / 100;
+      const receipt_url = event.data.object.receipt_url as string;
+      const data = await handleDonations(email, amount, receipt_url);
+      if (data.error) {
+        return context.json({ error: data.error }, 400);
+      }
+      break;
+    }
+
+    case 'customer.subscription.updated': {
+      const subscription = event.data.object;
+      const customer = (await stripe.customers.retrieve(subscription.customer as string)) as Stripe.Customer;
+      if (!customer) {
+        return context.json({ error: 'Customer not found' }, 400);
+      }
+      const email = customer?.email as string;
+
+      if (subscription.status === 'active') {
+        const invoice = await stripe.invoices.retrieve(subscription.latest_invoice as string);
+        const invoice_url = invoice.hosted_invoice_url as string;
+        const data = await handleSubscription(subscription.id, invoice_url, email);
+        if (data.error) {
+          return context.json({ error: data.error }, 400);
+        }
+      }
+
+      break;
+    }
+    case 'customer.subscription.deleted' || 'customer.subscription.paused': {
+      const subscription = event.data.object.id as string;
+      const user = await stripe.subscriptions.retrieve(subscription);
+
+      const data = await handleRevokeSubscription(user.customer as Stripe.Customer);
+      if (data.error) {
+        return context.json({ error: data.error }, 400);
+      }
+      break;
+    }
+    default:
+      break;
+  }
+  return context.json({ message: 'Received' }, 200);
 });
 
 stripe.openapi(listDonations, async (c) => {

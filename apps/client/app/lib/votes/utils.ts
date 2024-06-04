a278947b35ec2430065b7588a768e94fb7fd1ae4
Antony David <antonydavid945@gmail.com>
2024-06-04 22:13:49 +0200

    fix: votes pages + check user subscription on page load

--- apps/client/app/lib/votes/utils.ts
+++ apps/client/app/lib/votes/utils.ts
@@ -1,6 +1,6 @@
 import type { Vote } from '../type/Votes';
 
-export async function getAllVotes(): Promise<Vote[]> {
+export async function getAllVotes(): Promise<{ data: Vote[]; count: number }> {
   const API_URL = process.env.NEXT_PUBLIC_API_URL;
   const response = await fetch(`${API_URL}/polls?all=true`, {
     headers: { Authorization: `Bearer ${localStorage.getItem('access_token')}` },

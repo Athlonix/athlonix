5fe7b9b43c4723921cfef99d3cfcbe0a58f23707
Simon <szhang0709@gmail.com>
2024-06-07 16:52:18 +0200

    to continue

--- apps/client/app/(withNavbar)/tournaments/details/page.tsx
+++ apps/client/app/(withNavbar)/tournaments/details/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import TournamentCard from '@/app/ui/components/tournaments/TournamentCard';
 import { Button } from '@repo/ui/components/ui/button';
 import { Separator } from '@repo/ui/components/ui/separator';
 import { toast } from '@repo/ui/components/ui/sonner';
@@ -21,36 +20,60 @@ export type Tournament = {
   id_sport: number | null;
 };
 
+interface TeamsData {
+  data: Teams[];
+  count: number;
+}
+
+export type Teams = {
+  id: number;
+  name: string;
+};
+
 function ShowContent() {
   const searchParams = useSearchParams();
   const id = searchParams.get('id') || 1;
 
   const [tournament, setTournament] = useState<Tournament | null>(null);
+  const [teams, setTeams] = useState<Teams[]>([]);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      const urlApi = process.env.NEXT_PUBLIC_API_URL;
+    const urlApi = process.env.NEXT_PUBLIC_API_URL;
 
-      fetch(`${urlApi}/tournaments/${id}`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
+    fetch(`${urlApi}/tournaments/${id}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => {
+        if (response.status !== 200) {
+          throw new Error('Erreur lors de la récupération des tournois');
+        }
+        return response.json();
       })
-        .then((response) => {
-          if (response.status !== 200) {
-            throw new Error('Erreur lors de la récupération des tournois');
-          }
-          return response.json();
-        })
-        .then((data) => {
-          setTournament(data);
-          console.log(data);
-        })
-        .catch((error: Error) => toast.error(error.message, { duration: 5000 }));
-    }, 500);
+      .then((data) => {
+        setTournament(data);
+        console.log(data);
+      })
+      .catch((error: Error) => toast.error(error.message, { duration: 5000 }));
 
-    return () => clearTimeout(timer);
+    fetch(`${urlApi}/tournaments/${id}/teams`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => {
+        if (response.status !== 200) {
+          throw new Error('Erreur lors de la récupération des équipes');
+        }
+        return response.json();
+      })
+      .then((data: TeamsData) => {
+        setTeams(data.data);
+      })
+      .catch((error: Error) => toast.error(error.message, { duration: 5000 }));
   }, [id]);
 
   return (
@@ -87,6 +110,20 @@ function ShowContent() {
           </div>
         </>
       )}
+      <Separator className="my-8" />
+      <div className="flex justify-center w-full my-4">
+        <h2 className="font-bold">Equipes</h2>
+      </div>
+      {teams.length === 0 && (
+        <div className="flex justify-center w-full my-4">
+          <p>Aucune équipe inscrite</p>
+        </div>
+      )}
+      {teams.map((team) => (
+        <div key={team.id} className="grid gap-2 text-4xl mx-12">
+          <p>{team.name}</p>
+        </div>
+      ))}
       <Separator className="my-8" />
       <div className="flex justify-center w-full my-4">
         <Button>Participer</Button>

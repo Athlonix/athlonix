30a5c0803df1181c76a3ffd78a6347ee9ca17d76
MetooSHEESH <93775692+Metololo@users.noreply.github.com>
2024-06-07 09:10:18 +0200

    feat: fill seed sql with employees

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -18,3 +18,22 @@ VALUES
   (3, 'Informations mensongères ou glorification de la violence'),
   (4, 'Divulgation d''informations privé permettant d''identifier une personne'),
   (5, 'Autre raison');
+
+INSERT INTO public."USERS" (email, username, first_name, last_name, created_at)
+VALUES 
+('john.doe@example.com', 'johndoe_employee', 'John', 'Doe', NOW()),
+('jane.smith@example.com', 'janesmith_employee', 'Jane', 'Smith', NOW()),
+('michael.johnson@example.com', 'michaeljohnson_employee', 'Michael', 'Johnson', NOW()),
+('patricia.brown@example.com', 'patriciabrown_employee', 'Patricia', 'Brown', NOW()),
+('robert.jones@example.com', 'robertjones_employee', 'Robert', 'Jones', NOW()),
+('linda.garcia@example.com', 'lindagarcia_employee', 'Linda', 'Garcia', NOW()),
+('william.martinez@example.com', 'williammartinez_employee', 'William', 'Martinez', NOW()),
+('elizabeth.rodriguez@example.com', 'elizabethrodriguez_employee', 'Elizabeth', 'Rodriguez', NOW()),
+('charles.davis@example.com', 'charlesdavis_employee', 'Charles', 'Davis', NOW()),
+('barbara.lopez@example.com', 'barbaralopez_employee', 'Barbara', 'Lopez', NOW()),
+('joseph.miller@example.com', 'josephmiller_employee', 'Joseph', 'Miller', NOW());
+
+INSERT INTO public."USERS_ROLES" (id_user, id_role)
+SELECT id, 10
+FROM public."USERS"
+WHERE username IN ('johndoe_employee', 'janesmith_employee', 'michaeljohnson_employee', 'patriciabrown_employee', 'robertjones_employee', 'lindagarcia_employee', 'williammartinez_employee', 'elizabethrodriguez_employee', 'charlesdavis_employee', 'barbaralopez_employee', 'josephmiller_employee');

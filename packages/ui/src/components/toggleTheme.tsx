2078f347d0ece568cacf84477c4e451c59d0f982
Antony David <antonydavid945@gmail.com>
2024-06-09 19:19:03 +0200

    fix(ui): toggle theme imports

--- packages/ui/src/components/toggleTheme.tsx
+++ packages/ui/src/components/toggleTheme.tsx
@@ -1,12 +1,12 @@
 'use client';
 
-import { Button } from '@repo/ui/components/ui/button';
+import { Button } from '@ui/components/ui/button';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from '@repo/ui/components/ui/dropdown-menu';
+} from '@ui/components/ui/dropdown-menu';
 import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
 

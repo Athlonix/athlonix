7e325312a4145df3c4f6ca8640188da4743bf72b
Antony David <antonydavid945@gmail.com>
2024-06-09 20:05:52 +0200

    fix(admin): clean layout and links

--- apps/admin/app/page.tsx
+++ apps/admin/app/page.tsx
@@ -1,9 +1,17 @@
 import { LoginForm } from '@/app/ui/LoginForm';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Page(): JSX.Element {
   return (
     <main>
       <div className="flex items-center justify-center h-screen">
+        <div className="absolute top-2 left-0 mt-4 ml-4">
+          <Link href="/" className="flex items-center">
+            <Image src="/favicon.ico" alt="Accueil" width="32" height="32" />
+            <h2 className="ml-2 font-bold">Athlonix</h2>
+          </Link>
+        </div>
         <LoginForm />
       </div>
     </main>

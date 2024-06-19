93cf1da571e2a8100dcea212678d241ad0ea8ca9
Antony David <antonydavid945@gmail.com>
2024-06-19 15:54:44 +0200

    feat(client): rework edm page ui

--- apps/client/app/(auth)/(members)/members/documents/page.tsx
+++ apps/client/app/(auth)/(members)/members/documents/page.tsx
@@ -1,21 +1,121 @@
 'use client';
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@repo/ui/components/ui/table';
-import { Eye } from 'lucide-react';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from '@repo/ui/components/ui/breadcrumb';
+import { Card } from '@ui/components/ui/card';
+import { CircleArrowLeft, File, FileImage, FolderOpen } from 'lucide-react';
 import { useEffect, useState } from 'react';
-import { type Files, getAllFiles } from './utils';
+import { type Files, type Folders, getAllFiles } from './utils';
+
+function getParentFolders(folders: Folders[], folderId: number): Folders[] {
+  const folder = folders.find((f) => f.id === folderId);
+  if (!folder) {
+    return [];
+  }
+  if (!folder.parent) {
+    return [folder];
+  }
+  return [...getParentFolders(folders, folder.parent), folder];
+}
+
+function fileTypes(input: string) {
+  switch (input) {
+    case 'application/pdf' || 'application/x-pdf':
+      return 'PDF';
+    case 'application/msword' || 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+      return 'Word';
+    case input.includes('image') && input:
+      return 'Image';
+    default:
+      return 'Other';
+  }
+}
+
+function FolderItem({ name, onClick }: { name: string; onClick: () => void }) {
+  return (
+    <Card
+      className="m-2 p-2 cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-800 w-48 h-24 rounded-lg shadow-md border-2 border-gray-200 dark:border-gray-700"
+      onClick={onClick}
+    >
+      <div className="flex items-center justify-between mb-1">
+        <FolderOpen className="w-8" />
+      </div>
+      <h2>{name}</h2>
+    </Card>
+  );
+}
+
+function DisplayFolderAndFiles({
+  folder,
+  onFolderClick,
+  files,
+  onFileClick,
+  currrentPath,
+}: {
+  folder: Folders[] | null;
+  onFolderClick: (name: string) => void;
+  files: Files[] | null;
+  onFileClick: (id: number) => void;
+  currrentPath: number | null;
+}) {
+  return (
+    <>
+      {currrentPath === null
+        ? folder
+            ?.filter((f) => !f.parent)
+            .map((f) => <FolderItem key={f.id} name={f.name} onClick={() => onFolderClick(f.name)} />)
+        : folder
+            ?.filter((f) => f.parent === currrentPath)
+            .map((f) => <FolderItem key={f.id} name={f.name} onClick={() => onFolderClick(f.name)} />)}
+
+      {files
+        ?.filter((f) => f.folder === currrentPath)
+        .map((f) => (
+          <Card
+            key={f.id}
+            className={
+              'w-64 m-2 p-4 cursor-pointer transition-colors duration-300 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-lg shadow-md border-2 border-gray-200 dark:border-gray-700'
+            }
+            onClick={() => onFileClick?.(f.id)}
+          >
+            <div className="flex items-center space-x-2 mb-2">
+              {fileTypes(f.type) === 'Image' ? <FileImage /> : <File />}
+            </div>
+            <div className="flex justify-between items-center">
+              <h2 className="text-lg font-semibold pr-4">{f.name}</h2>
+            </div>
+            <p className="text-sm text-gray-700 dark:text-gray-300 mt-1">
+              {f.description.length > 25 ? `${f.description.slice(0, 25)}...` : f.description}
+            </p>
+            <p className="text-sm text-gray-500 mt-2">{fileTypes(f.type)}</p>
+            <p className="text-xs text-gray-400 mt-2">
+              {`${new Date(f.created_at).toLocaleDateString()} ${new Date(f.created_at).toLocaleTimeString()}`}
+            </p>
+          </Card>
+        ))}
+    </>
+  );
+}
 
 export default function Documents() {
-  const [files, setFiles] = useState<{ data: Files[]; count: number } | null>(null);
+  const [files, setFiles] = useState<Files[] | null>(null);
+  const [folders, setFolders] = useState<Folders[] | null>(null);
+  const [currrentPath, setCurrentPath] = useState<number | null>(null);
 
   useEffect(() => {
-    const getFIles = async () => {
-      const files = await getAllFiles();
-      setFiles(files);
+    const getFiles = async () => {
+      const data = await getAllFiles();
+      setFiles(data.files);
+      setFolders(data.folders);
     };
-    getFIles();
+    getFiles();
   }, []);
 
-  async function viewFile(id: number) {
+  async function viewFile(id: number): Promise<void> {
     const API_URL = process.env.NEXT_PUBLIC_API_URL;
     const token = localStorage.getItem('access_token');
     const response = await fetch(`${API_URL}/edm/download/${id}`, {
@@ -32,17 +132,9 @@ export default function Documents() {
     window.open(url, '_blank');
   }
 
-  function fileTypes(input: string) {
-    switch (input) {
-      case 'application/pdf' || 'application/x-pdf':
-        return 'PDF';
-      case 'application/msword' || 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
-        return 'Word';
-      case input.includes('image') && input:
-        return 'Image';
-      default:
-        return 'Other';
-    }
+  function onFolderClick(name: string): void {
+    const folder = folders?.find((f) => f.name === name);
+    setCurrentPath(folder?.id || null);
   }
 
   return (
@@ -50,36 +142,42 @@ export default function Documents() {
       <header className="bg-gray-100 dark:bg-gray-800 px-6 py-4 flex items-center justify-between">
         <h1 className="text-xl font-bold">Documents d'Athlonix</h1>
       </header>
-      <div className="flex-1 overflow-auto p-6">
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Nom</TableHead>
-              <TableHead>Description</TableHead>
-              <TableHead>Type</TableHead>
-              <TableHead>Uploaded</TableHead>
-              <TableHead>Updated</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {files?.data.map((file) => (
-              <TableRow key={file.id}>
-                <TableCell>{file.name}</TableCell>
-                <TableCell>{file.description || 'Aucune description'}</TableCell>
-                <TableCell>{fileTypes(file.type)}</TableCell>
-                <TableCell>{`${new Date(file.created_at).toLocaleDateString()} ${new Date(
-                  file.created_at,
-                ).toLocaleTimeString()}`}</TableCell>
-                <TableCell>{`${new Date(file.updated_at).toLocaleDateString()} ${new Date(
-                  file.updated_at,
-                ).toLocaleTimeString()}`}</TableCell>
-                <TableCell className="flex gap-2">
-                  <Eye className="cursor-pointer" onClick={() => viewFile(file.id)} />
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+      <div className="flex items-center space-x-2 p-4">
+        <CircleArrowLeft
+          className="w-8 h-8"
+          onClick={() => {
+            setCurrentPath(currrentPath ? folders?.find((f) => f.id === currrentPath)?.parent || null : null);
+          }}
+        />
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem className="cursor-pointer">
+              <BreadcrumbLink onClick={() => setCurrentPath(null)}>Documents</BreadcrumbLink>
+            </BreadcrumbItem>
+            {currrentPath !== null && (
+              <>
+                {folders?.map &&
+                  getParentFolders(folders, currrentPath).map((f) => (
+                    <>
+                      <BreadcrumbSeparator key={`separator-${f.id}`} />
+                      <BreadcrumbItem key={f.id} className="cursor-pointer">
+                        <BreadcrumbLink onClick={() => setCurrentPath(f.id)}>{f.name}</BreadcrumbLink>
+                      </BreadcrumbItem>
+                    </>
+                  ))}
+              </>
+            )}
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+      <div className="flex flex-wrap">
+        <DisplayFolderAndFiles
+          folder={folders}
+          onFolderClick={onFolderClick}
+          files={files}
+          onFileClick={viewFile}
+          currrentPath={currrentPath}
+        />
       </div>
     </div>
   );
--- apps/client/app/(auth)/(members)/members/documents/utils.ts
+++ apps/client/app/(auth)/(members)/members/documents/utils.ts
@@ -1,6 +1,15 @@
 'use server';
 import { cookies } from 'next/headers';
 
+export type Folders = {
+  id: number;
+  name: string;
+  parent: number | null;
+  creator: number;
+  isAdmin: boolean;
+  created_at: string;
+};
+
 export type Files = {
   id: number;
   name: string;
@@ -10,29 +19,16 @@ export type Files = {
   updated_at: string;
   created_at: string;
   type: string;
+  assembly: number | null;
+  folder: Folders | null;
 };
 
-export async function saveFile(form: FormData): Promise<void> {
-  const API_URL = process.env.ATHLONIX_API_URL;
-  const token = cookies().get('access_token')?.value;
-  if (form.get('isAdmin') === 'on') {
-    form.set('isAdmin', 'true');
-  } else {
-    form.set('isAdmin', 'false');
-  }
-  const response = await fetch(`${API_URL}/edm/upload`, {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-    body: form,
-  });
-  if (!response.ok) {
-    throw new Error('Failed to save file');
-  }
-}
-
-export async function getAllFiles(): Promise<{ data: Files[]; count: number }> {
+export async function getAllFiles(): Promise<{
+  files: Files[];
+  countFiles: number;
+  folders: Folders[];
+  countFolders: number;
+}> {
   const API_URL = process.env.ATHLONIX_API_URL;
   const token = cookies().get('access_token')?.value;
   const response = await fetch(`${API_URL}/edm/listFiles?all=true`, {
@@ -43,41 +39,6 @@ export async function getAllFiles(): Promise<{ data: Files[]; count: number }> {
   if (!response.ok) {
     throw new Error('Failed to fetch files');
   }
-  return await response.json();
-}
-
-export async function deleteFile(id: number, name: string): Promise<void> {
-  const API_URL = process.env.ATHLONIX_API_URL;
-  const token = cookies().get('access_token')?.value;
-  const response = await fetch(`${API_URL}/edm/delete`, {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify({ id, name }),
-  });
-  if (!response.ok) {
-    throw new Error('Failed to delete file');
-  }
-}
-
-export async function updateFile(form: FormData, id: number): Promise<void> {
-  const API_URL = process.env.ATHLONIX_API_URL;
-  const token = cookies().get('access_token')?.value;
-  if (form.get('isAdmin') === 'on') {
-    form.set('isAdmin', 'true');
-  } else {
-    form.set('isAdmin', 'false');
-  }
-  const response = await fetch(`${API_URL}/edm/update/${id}`, {
-    method: 'PATCH',
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-    body: form,
-  });
-  if (!response.ok) {
-    throw new Error('Failed to update file');
-  }
+  const data = await response.json();
+  return { files: data.files, countFiles: data.filesCount, folders: data.folders, countFolders: data.foldersCount };
 }

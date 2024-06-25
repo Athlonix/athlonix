c26430ce15f4c6b713ad98f599116576ba05ed3f
Antony DAVID <antony.david@urbanandmainlines.com>
2024-06-25 14:34:51 +0200

    ci: add api code coverage

--- apps/api/vitest.config.ts
+++ apps/api/vitest.config.ts
@@ -6,5 +6,18 @@ export default defineConfig({
     globals: true,
     maxWorkers: 1,
     minWorkers: 1,
+    coverage: {
+      provider: 'v8',
+      thresholds: {
+        global: {
+          statements: 20,
+          branches: 20,
+          functions: 20,
+          lines: 20,
+        },
+      },
+      reporter: ['text', 'html', 'json', 'json-summary'],
+      reportOnFailure: true,
+    },
   },
 });

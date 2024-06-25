6f9d750ce79e4fec1c564a62be4bbdafadc7be7a
Antony DAVID <antony.david@urbanandmainlines.com>
2024-06-25 14:40:04 +0200

    fix: ci

--- apps/api/vitest.config.ts
+++ apps/api/vitest.config.ts
@@ -8,14 +8,6 @@ export default defineConfig({
     minWorkers: 1,
     coverage: {
       provider: 'v8',
-      thresholds: {
-        global: {
-          statements: 20,
-          branches: 20,
-          functions: 20,
-          lines: 20,
-        },
-      },
       reporter: ['text', 'html', 'json', 'json-summary'],
       reportOnFailure: true,
     },

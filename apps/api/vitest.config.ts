081c62bbe83a783798142edf7f7449d0cf3101ce
Antony DAVID <antony.david@urbanandmainlines.com>
2024-06-25 14:44:26 +0200

    fix: node warnings  & thresholds

--- apps/api/vitest.config.ts
+++ apps/api/vitest.config.ts
@@ -8,6 +8,14 @@ export default defineConfig({
     minWorkers: 1,
     coverage: {
       provider: 'v8',
+      thresholds: {
+        global: {
+          statements: 70,
+          branches: 30,
+          functions: 40,
+          lines: 70,
+        },
+      },
       reporter: ['text', 'html', 'json', 'json-summary'],
       reportOnFailure: true,
     },

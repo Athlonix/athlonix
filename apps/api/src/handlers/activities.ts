b788fe8f38551e0852cc224ea3e3259a37c5f671
Simon ZHANG <102216594+userMeh@users.noreply.github.com>
2024-07-19 18:23:24 +0200

    fix: can't join activity

--- apps/api/src/handlers/activities.ts
+++ apps/api/src/handlers/activities.ts
@@ -337,7 +337,8 @@ activities.openapi(applyToActivity, async (c) => {
   const { data: activity, error: errorActivity } = await supabase.from('ACTIVITIES').select('*').eq('id', id).single();
 
   if (errorActivity || !activity) return c.json({ error: 'Activity not found' }, 404);
-  if (new Date(date) < new Date()) return c.json({ error: 'Activity has already ended' }, 400);
+  if (new Date(`${date}T${activity.start_time}`) < new Date())
+    return c.json({ error: 'Activity has already ended' }, 400);
 
   const { count } = await supabase
     .from('ACTIVITIES_USERS')
